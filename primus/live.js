0401beb2232147e7f31f7b22c225972672a79c56
Joris Hens <joris@goodbytes.be>
2018-03-18 20:54:09 +0100

    Working example implemented 🚀

--- primus/live.js
+++ primus/live.js
@@ -3,12 +3,9 @@ exports.go = function(server) {
     var primus = new Primus(server, {});
 
     primus.on('connection', function connection(spark) {
-        console.log("Primus server connection 🐴");
-        
-        primus.write('Hey sparks, here is some data! ☕️');
 
         spark.on('data', function (data) {
-            console.log("Data from spark ⚡️")
+            primus.write(data);
         });
 
     });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,5 +8,42 @@ var primus = Primus.connect(url, {
 });
 
 primus.on('data', function message(data) {
-    console.log('Received a new message from the server', data);
+    // console.log('Received a new message from the server', data);
+    var liveBoard = document.querySelector(".emoji__live");
+    if(liveBoard){
+        console.log(data);
+        var emotion = data.emotion;
+        var emotionElement = document.querySelector(".emo__"+emotion);
+        var zoom = emotionElement.dataset.zoom;
+        if(zoom < 500) {
+            var newFontPercentage = zoom * 1.04;
+            emotionElement.style.fontSize = newFontPercentage + "%";
+            emotionElement.dataset.zoom = newFontPercentage;
+        }        
+    }
 });
+
+// clicking on an emoji should send an even to the primus server
+if(document.querySelector(".emoji__home")){
+    document.querySelector(".emoji__home").addEventListener("click", function(e){
+        
+        var lastVote = localStorage.getItem("voted");
+        if(lastVote){
+            var lastVoteDate = new Date(lastVote);
+            var now = new Date();
+            var diff = parseInt( (now-lastVoteDate) / 1000 / 60 ) ;
+            if(diff < 5)
+            {
+                //return false;
+            }
+        }
+        
+        localStorage.setItem('voted', new Date());
+        var target = e.target;
+        var emotion = target.dataset.emo;
+        if(emotion != undefined){
+            primus.write({ emotion: emotion });
+        }
+        e.preventDefault();
+    });
+}
