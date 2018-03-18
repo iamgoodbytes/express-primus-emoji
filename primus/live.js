76e79c9c30f17dbf11b2ccef04af4ad4b1548e73
Joris Hens <joris@goodbytes.be>
2018-03-18 17:36:06 +0100

    Set up base Primus client/server communication

--- primus/live.js
+++ primus/live.js
@@ -2,5 +2,15 @@ exports.go = function(server) {
     const Primus = require('primus');
     var primus = new Primus(server, {});
 
-    
+    primus.on('connection', function connection(spark) {
+        console.log("Primus server connection 🐴");
+        
+        primus.write('Hey sparks, here is some data! ☕️');
+
+        spark.on('data', function (data) {
+            console.log("Data from spark ⚡️")
+        });
+
+    });
+
 }
---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+var url = "/";
+var primus = Primus.connect(url, {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+      , min: 500 // Number: The minimum delay before we try reconnect.
+      , retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
+primus.on('data', function message(data) {
+    console.log('Received a new message from the server', data);
+});
